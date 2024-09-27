8008d01e778d58da20f33fb1af38d002a5266b88
Ayodele Owolabi <joyowolabi@gmail.com>
2024-09-27 16:28:27 -0400

    finished Django part 2 lab

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('new', views.new, name='new')
+    # path('new', views.new, name='new'),
+    path('about/', views.about, name='about'),
+    path('characters/', views.character_index, name='character-index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+
+class Character:
+    def __init__(self, name, description, age):
+        self.name = name,
+        self.description = description,
+        self.age = age
+        
+characters = [
+    Character('Mario', 'Red and blue', 47),
+    Character('Luigi', 'Green', 43),
+    Character('Princess Peach', 'Pink', 23),
+    Character('Toad', 'Blue and Brown', 34),
+    Character('Bowser', 'Brown', 47),
+    Character('Donkey', 'Brown', 17)
+    ]
 
 
 # Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello I work!<h1>')
+    return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
 
-def new(request):
-    return HttpResponse('<h2> New Page <h2>')
+def character_index(request):
+    return render(request,'characters/index.html', {'characters':characters})
